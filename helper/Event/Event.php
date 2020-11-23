dbdc3cb390787ea9c7d2aaee2942864adfd140a1
hepeichun <946599101@qq.com>
2020-11-23 18:10:55 +0800

    style:优化事件事件响应机制

--- helper/Event/Event.php
+++ helper/Event/Event.php
@@ -4,6 +4,8 @@
 namespace helper\Event;
 
 
+use DI\DependencyException;
+use DI\NotFoundException;
 use helper\Di;
 use interfaces\Console\OutputInterface;
 use function DI\value;
@@ -14,29 +16,38 @@ class Event
      * 事件触发
      * @param $event_name
      * @param $context
-     * @throws \DI\DependencyException
-     * @throws \DI\NotFoundException
      */
     public static function process($event_name,$context)
     {
-        $output = Di::getContainer()->get(OutputInterface::class);
-        $listener = Di::getContainer()->get(Listener::class);
-        $event_action = $listener->getEventAction($event_name);
-        foreach ($event_action as $key => $value) {
-            [$class,$action] = explode('@',$value['event_action']);
-            try {
-                $obj = new $class;
-                call_user_func([$obj,$action],$context);
-                unset($obj);
-            } catch (\Error $exception){
-                $output->writeln("事件:{$event_name},调用响应:{$value}失败");
-            } catch (\ErrorException $exception){
-                $output->writeln("事件:{$event_name},调用响应:{$value}失败");
-            } catch (\Exception $exception){
-                $output->writeln("事件:{$event_name},调用响应:{$value}失败");
-            } catch (\Throwable $exception){
-                $output->writeln("事件:{$event_name},调用响应:{$value}失败");
+        self::run("{$event_name}_before",$context);
+        self::run("{$event_name}",$context);
+        self::run("{$event_name}_after",$context);
+    }
+
+    private static function run($event_name,$context)
+    {
+
+        try {
+            $output = Di::getContainer()->get(OutputInterface::class);
+            $listener = Di::getContainer()->get(Listener::class);
+            $event_action = $listener->getEventAction($event_name);
+            foreach ($event_action as $key => $value) {
+                [$class,$action] = explode('@',$value['event_action']);
+                try {
+                    $obj = new $class;
+                    call_user_func([$obj,$action],$context);
+                    unset($obj);
+                }  catch (\Throwable $exception){
+                    $output->writeln("事件:{$event_name},调用响应:{$value}失败");
+                }
             }
+        } catch (DependencyException $e) {
+            var_dump($e->getMessage());
+        } catch (NotFoundException $e) {
+            var_dump($e->getMessage());
+        } catch (\Throwable $exception){
+            $output->writeln("事件:{$event_name},调用响应:{$value}失败");
         }
+
     }
 }
