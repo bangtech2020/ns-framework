22d35597d554a7ae688c751b8d5caedd93486a5b
hepeichun <946599101@qq.com>
2020-11-27 14:20:07 +0800

    add:添加事件监听

--- config/event.php
+++ config/event.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'app_start'=>[
-        \eventListener\AppStartEventListener::class
+        \event\AppStartEventListener::class
     ],
     'app_stop'=>[
-        \eventListener\AppStopEventListener::class
+        \event\AppStopEventListener::class
     ]
 ];
